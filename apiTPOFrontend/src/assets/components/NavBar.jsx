09b43159100fd7904b7579616821b0c13f3d9555
JuanmaGrispo <145155051+JuanmaGrispo@users.noreply.github.com>
2024-07-03 14:17:27 -0300

    starting to code navBar

---
+++ apiTPOFrontend/src/assets/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <>
+
+      <nav className='navbar'>
+        <Link>inicio</Link>
+        <Link>productos</Link>
+        <Link>contacto</Link>
+        <Link>configuración</Link>
+
+
+      </nav>
+    </>
+  )
+}
+
